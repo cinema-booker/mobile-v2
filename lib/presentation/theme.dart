3352bc7a226b4ef792eb38dffa497997973145bb
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2024-07-12 01:01:38 +0200

    style: add assets and theme manager

---
+++ lib/presentation/theme.dart
@@ -0,0 +1,7 @@
+import 'package:cinema_booker/presentation/theme_color.dart';
+import 'package:flutter/material.dart';
+
+ThemeData appTheme = ThemeData(
+  fontFamily: 'Inter',
+  scaffoldBackgroundColor: ThemeColor.brown300,
+);
---
+++ lib/presentation/theme_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  static const Color brown100 = Color(0xFF4F4F4F);
+  static const Color brown200 = Color(0xFF262626);
+  static const Color brown300 = Color(0xFF1D1D1F);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color yellow = Color(0xFFE5F789);
+  static const Color gray = Color(0xFF959595);
+}
